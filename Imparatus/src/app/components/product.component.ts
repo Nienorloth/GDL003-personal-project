7b592bc1dddd995e5cead2e7da3757ce934d3111
Nienorloth <nienorloth@gmail.com>
2019-10-24 16:43:30 -0500

    cambiando a angular

--- Imparatus/src/app/components/product.component.ts
+++ Imparatus/src/app/components/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from '../product.model';
 
 @Component({
     selector: 'app-product',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 
 export class ProductComponent {
 
+   @Input() product: Product;
 }
