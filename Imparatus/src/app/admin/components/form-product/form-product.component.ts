1ac9872c9e97e929f907fb921e6a25ce855425a3
Nienorloth <nienorloth@gmail.com>
2019-11-09 07:57:50 -0600

    separated back end

--- Imparatus/src/app/admin/components/form-product/form-product.component.ts
+++ Imparatus/src/app/admin/components/form-product/form-product.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { ProductsService } from './../../../core/services/products/products.service';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -11,11 +14,13 @@ import { ProductsService } from './../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
   private FormBuilder: FormBuilder,
   private productsService: ProductsService,
-  private Router: Router
+  private Router: Router,
+  private storage: AngularFireStorage
   ) {
     this.BuildForm();
   }
@@ -33,6 +38,25 @@ export class FormProductComponent implements OnInit {
         this.Router.navigate(['./admin/products']);
       })
     }
+  }
+
+  uploadFile(event){
+    const file = event.target.files[0];
+    const name = 'image.jpg';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image').setValue(url);
+        })
+      })
+    )
+    .subscribe();
   }
 
   private BuildForm() {
